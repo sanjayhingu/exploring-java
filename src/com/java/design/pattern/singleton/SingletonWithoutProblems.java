92b6d96e9d22fbdb1033a74e2b1b94c1ac95bc3b
sanjayhingu <sanjay.hingu@gmail.com>
2017-07-02 22:23:57 +0530

    Singleton pattern

--- src/com/java/design/pattern/singleton/SingletonWithoutProblems.java
+++ src/com/java/design/pattern/singleton/SingletonWithoutProblems.java
@@ -1,21 +1,25 @@
 package com.java.design.pattern.singleton;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.lang.reflect.Constructor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
+/**
+ * Implementation of Singleton design pattern which will handle following
+ * scenarios.
+ * 
+ * 1. Multi-threading
+ * 2. Cloneable
+ * 3. Serializable
+ * 4. Reflection
+ * 
+ * 
+ * @author Sanjay Hingu
+ */
 public class SingletonWithoutProblems implements Serializable, Cloneable {
-	
-	private static final long serialVersionUID = -478011518780485908L;
+
 	private static volatile SingletonWithoutProblems singleton;
-	
+
 	private SingletonWithoutProblems() throws Exception {
-		if(SingletonWithoutProblems.singleton!=null) {
+		if (SingletonWithoutProblems.singleton != null) {
 			throw new RuntimeException("Cannot create instance");
 		} else {
 			this.singleton = this;
@@ -23,90 +27,23 @@ public class SingletonWithoutProblems implements Serializable, Cloneable {
 	}
 
 	public static SingletonWithoutProblems getInstance() throws Exception {
-		if(singleton == null) {
+		if (singleton == null) {
 			synchronized (SingletonWithProblems.class) {
-				if(singleton == null) {
+				if (singleton == null) {
 					singleton = new SingletonWithoutProblems();
 				}
 			}
 		}
 		return singleton;
 	}
-	
+
 	private Object readResolve() {
 		return singleton;
 	}
-	
+
 	@Override
 	public Object clone() {
 		return singleton;
 	}
-	
-	public static void main(String[] args){}
-}
 
-class TestSingletonWithoutProblems  {
-	
-	public static void testReflection() throws Exception {
-		Class singletonClass = Class.forName("com.java.design.pattern.singleton.SingletonWithoutProblems");
-		Constructor<SingletonWithoutProblems> constructor = singletonClass.getDeclaredConstructor();
-		constructor.setAccessible(Boolean.TRUE);
-		SingletonWithoutProblems s3 = constructor.newInstance();
-		System.out.println(s3.hashCode());
-		
-		SingletonWithoutProblems s1 = SingletonWithoutProblems.getInstance();
-		SingletonWithoutProblems s2 = SingletonWithoutProblems.getInstance();
-		
-		System.out.println(s1.hashCode());
-		System.out.println(s2.hashCode());
-		
-		SingletonWithoutProblems s4 = null;
-		try {
-			s4 = constructor.newInstance();
-			System.out.println(s4.hashCode());
-			throw new RuntimeException("Singleton instance shouldn't have been created");
-		} catch(Exception e) {}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		testReflection();
-		testSingleton();
-		testSerializable();
-		testCloaneable();
-	}
-
-	private static void testCloaneable() throws Exception {
-		SingletonWithoutProblems s1 = SingletonWithoutProblems.getInstance();
-		System.out.println(s1.hashCode());
-		SingletonWithoutProblems s2 = (SingletonWithoutProblems) s1.clone();
-		System.out.println(s2.hashCode());
-	}
-
-	private static void testSerializable() throws Exception {
-		SingletonWithoutProblems s1 = SingletonWithoutProblems.getInstance();
-		System.out.println(s1.hashCode());
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singletonwithoutprob.ser"));
-		oos.writeObject(s1);
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("singletonwithoutprob.ser"));
-		SingletonWithoutProblems s2 = (SingletonWithoutProblems) ois.readObject();
-		System.out.println(s2.hashCode());
-	}
-
-	private static void useSingleton() {
-		SingletonWithoutProblems singleton = null;
-		try {
-			singleton = SingletonWithoutProblems.getInstance();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("singleton hascode: "+singleton.hashCode());
-	}	
-	
-	public static void testSingleton() throws Exception{
-		ExecutorService service = Executors.newFixedThreadPool(2);
-		service.submit(TestSingletonWithoutProblems::useSingleton);
-		service.submit(TestSingletonWithoutProblems::useSingleton);
-		service.shutdown();
-	}
 }
---
+++ src/com/java/design/pattern/singleton/TestSingletonWithoutProblems.java
@@ -0,0 +1,67 @@
+package com.java.design.pattern.singleton;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSingletonWithoutProblems {
+
+	@Test (expected = InvocationTargetException.class)
+	public void testReflection() throws Exception {
+		SingletonWithoutProblems s1 = SingletonWithoutProblems.getInstance();
+		SingletonWithoutProblems s2 = SingletonWithoutProblems.getInstance();
+		Assert.assertEquals("Singleton design pattern failed", s1, s2);
+		
+		Class singletonClass = Class.forName("com.java.design.pattern.singleton.SingletonWithoutProblems");
+		Constructor<SingletonWithoutProblems> constructor = singletonClass.getDeclaredConstructor();
+		constructor.setAccessible(Boolean.TRUE);
+		SingletonWithoutProblems s3 = constructor.newInstance();
+	}
+	
+	@Test
+	public void testCloneable() throws Exception {
+		SingletonWithoutProblems s1 = SingletonWithoutProblems.getInstance();
+		SingletonWithoutProblems s2 = (SingletonWithoutProblems) s1.clone();
+		Assert.assertEquals("Singleton pattern failed Cloneable test", s1, s2);
+	}
+	
+	@Test
+	public void testSerializable() throws Exception {
+		SingletonWithoutProblems s1 = SingletonWithoutProblems.getInstance();
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singletonwithoutprob.ser"));
+		oos.writeObject(s1);
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("singletonwithoutprob.ser"));
+		SingletonWithoutProblems s2 = (SingletonWithoutProblems) ois.readObject();
+		Assert.assertEquals("Singleton pattern failed serializable test", s1, s2);
+	}
+	
+	private static SingletonWithoutProblems useSingleton() {
+		SingletonWithoutProblems singleton = null;
+		try {
+			singleton = SingletonWithoutProblems.getInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return singleton;
+	}	
+	
+	@Test
+	public void testMultithreading() throws Exception {
+		ExecutorService service = Executors.newFixedThreadPool(2);
+		Future<SingletonWithoutProblems> s1 = service.submit(TestSingletonWithoutProblems::useSingleton);
+		Future<SingletonWithoutProblems> s2 = service.submit(TestSingletonWithoutProblems::useSingleton);
+		service.shutdown();
+		Assert.assertEquals("Singleton pattern failed multithreading test", s1.get(), s2.get());
+	}
+	
+}
